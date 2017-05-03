791636cf87278783379cc8ae8242ad85dcf72451
Erica Solum <erica.solum@yahoo.com>
2017-05-02 19:27:02 -0700

    All the combined things yay!

--- FarmWorld.java
+++ FarmWorld.java
@@ -29,6 +29,16 @@ public class FarmWorld extends World
     public void act() {
         frameCount++;
         
+        if(Greenfoot.mouseClicked(this))
+        {
+            Level3 level3 = new Level3();
+            Greenfoot.setWorld(level3);
+            //MouseInfo mouse=Greenfoot.getMouseInfo();
+            //int mX=mouse.getX(), mY=mouse.getY();
+            // with text top at 80, bottom at 100, left at 350, and right at 450
+            //if(mX>=350 && mX<=450 && mY>=80 && mY<=100) methodName();
+        }
     }
+   
     
 }
